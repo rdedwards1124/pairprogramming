3345b4547a041d3ec455ee553f6cc563310c6be3
Rashonda Edwards <rashondaedwards@aol.com>
2024-04-17 17:43:34 -0700

    carly's workshop

---
+++ Workshops/Workshop-041724.js
@@ -0,0 +1,28 @@
+
+const synonyms = ['fantastic', 'wonderful', 'great']
+const greetings = []
+
+// 1.Intialing counter variable, 2.Setting conditional, 3.Running instructional, 4.Increment the counter
+// Template literal: ${ }
+for(let i = 0; i < synonyms.length; i++){
+    // Push each syn into greeting
+
+}
+
+/////////////////////////////////////////////////////
+
+const x = 3
+
+function isX1 (num){
+    const x = 5
+    return num === x
+}
+
+function isX2 (num){
+    return num === x
+}
+
+const one = isX1(5)
+const two = isX2(3)
+
+// Declaring a constant variable and setting it equal to the Evaluated result of isX1 with an argument of 5.
